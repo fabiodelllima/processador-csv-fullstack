3d79ab2c422aea73fa86887e95b1fc3e64612ac1
fabiodelllima <f.f.fabiodelima@gmail.com>
2024-11-06 03:31:05 -0300

    fix(backend): Correção em imports após reestruturação do projeto

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,8 @@ import express, { Express, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import { csvRoutes } from "./routes/csvRoutes.routes";
-import { errorHandler } from "./middlewares/errorHandler";
+import { csvRoutes } from "./routes/csv.route";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
 
 dotenv.config();
 
