f1e611fe80dda4b83aec44e9010746d343c72365
fabiodelllima <f.f.fabiodelima@gmail.com>
2024-11-08 02:30:07 -0300

    refactor(frontend): Refinamento em botão desabilitado

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -23,6 +23,8 @@ export const Home = () => {
     processFile,
   } = useCsvProcessor();
 
+  const isButtonDisabled =
+    !file || isUploading || (file && file.type !== "text/csv");
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100">
       <div className="max-w-7xl mx-auto p-4 sm:p-6">
@@ -56,11 +58,13 @@ export const Home = () => {
             </div>
             <button
               onClick={processFile}
-              disabled={
-                !file || isUploading || (file && file.type !== "text/csv")
-              }
-              className="h-12 bg-blue-600/90 text-gray-100 rounded-lg hover:bg-blue-700/90 
-                       disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-200"
+              disabled={isButtonDisabled}
+              className={`h-12 bg-blue-600/90 text-gray-100 rounded-lg transition-all duration-200
+                        ${
+                          isButtonDisabled
+                            ? "opacity-50 cursor-default"
+                            : "hover:bg-blue-700/90 cursor-pointer"
+                        }`}
             >
               {isUploading ? "Enviando..." : "Enviar arquivo"}
             </button>
