390d8e8f4be527c34a839210593fcc13e871f8fc
fabiodelllima <fabiodelllima@gmail.com>
2026-04-06 16:17:42 -0300

    fix(frontend): Substitui  URL hardcoded por variável de ambiente VITE_API_URL

--- frontend/src/services/csv.service.ts
+++ frontend/src/services/csv.service.ts
@@ -1,11 +1,13 @@
 import { ResultData } from "../interfaces/csv.interface";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000"\;
+
 export const csvService = {
   upload: async (file: File) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await fetch("http://localhost:3000/api/csv/upload", {
+    const response = await fetch(`${API_URL}/api/csv/upload`, {
       method: "POST",
       body: formData,
     });
@@ -14,14 +16,14 @@ export const csvService = {
 
   checkStatus: async (processId: string) => {
     const response = await fetch(
-      `http://localhost:3000/api/csv/status/${processId}`
+      `${API_URL}/api/csv/status/${processId}`
     );
     return response.json();
   },
 
   getResults: async (processId: string): Promise<ResultData> => {
     const response = await fetch(
-      `http://localhost:3000/api/csv/download/${processId}`
+      `${API_URL}/api/csv/download/${processId}`
     );
     return response.json();
   },
