2ca6bda5acc55a8c602e25541d9a737941a4c113
fabiodelllima <fabiodelllima@gmail.com>
2026-04-06 19:15:55 -0300

    chore(backend): Configura ESLint type-aware e scripts de lint

---
+++ backend/eslint.config.js
@@ -0,0 +1,26 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": "error",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/require-await": "error",
+      "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+      "@typescript-eslint/strict-boolean-expressions": "warn",
+    },
+  },
+];
